6cc23e003c188d3d2eb3f5e3b32f35d28cc2670f
Masayuki Kondo <komasayuki@gmail.com>
2020-02-27 14:32:46 +0900

    init

---
+++ src/hello.ts
@@ -0,0 +1,17 @@
+export class Hello{
+
+    private name:string;
+
+    constructor(name:string){
+        this.name = name;
+    }
+
+    greeting():void{
+        console.log('Hello', this.name);
+    }
+
+    getName():string{
+        return this.name;
+    }
+
+}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import {Hello} from './hello'
+
+const hello = new Hello('World');
+hello.greeting();
